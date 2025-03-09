e8cd4de18b355e7994c90b753bfc35b4aa580606
Surendran Manokaran <surendranmelb24@gmail.com>
2025-03-10 07:39:16 +1100

    Set up Mongo DB connection and created src folder

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from "cors";
+import mongoose from 'mongoose';
+
+dotenv.config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGODB_CONNECTION_STRING)
+    .then(() => console.log("Connected to database"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+/**
+ * Server Health Check
+ */
+app.get("/health", async (req, res) => {
+    res.send({ message: "health OK!" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
