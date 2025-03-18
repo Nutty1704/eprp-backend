6f651eb0f4d1814623e586bb1909a3cf4c29bac1
Abhijit Upadhyay <abhijitupadhyay03@gmail.com>
2025-03-18 22:17:41 +1100

    Fix bug: google auth doesn't correctly create a new role account for an existing user. Modify google auth config to extract user names for owner account.

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -10,147 +10,113 @@ import Owner from "../models/user/owner.model.js";
 import { alreadyExists, createRoleEntity, isValidUserRole } from "./user-helper.js";
 
 import dotenv from "dotenv";
+import { InvalidRoleError } from "./error-utils.js";
 
 dotenv.config();
 
 // Local Strategy for authenticating users
 passport.use(
-    new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
-        try {
-            const user = await User.findOne({ email });
-            if (!user) return done(null, false, { message: "User not found" });
+  new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return done(null, false, { message: "User not found" });
 
-            const isMatch = await bcryptjs.compare(password, user.password);
-            if (!isMatch) return done(null, false, { message: "Incorrect credentials" });
+      const isMatch = await bcryptjs.compare(password, user.password);
+      if (!isMatch) return done(null, false, { message: "Incorrect credentials" });
 
-            const roles = await fetchUserRoles(user._id);
+      const roles = await fetchUserRoles(user._id);
 
-            if (Object.keys(roles).length === 0) {
-                return done(null, false, { message: "User role entity not found" });
-            }
+      if (Object.keys(roles).length === 0) {
+        return done(null, false, { message: "User role entity not found" });
+      }
 
-            return done(null, { ...user.toObject(), roles }); // Attach roles
-        } catch (error) {
-            return done(error);
-        }
-    })
+      return done(null, { ...user.toObject(), roles }); // Attach roles
+    } catch (error) {
+      return done(error);
+    }
+  })
 );
-
-// passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//             callbackURL: "/api/auth/google/callback",
-//             scope: ["profile", "email"],
-//             passReqToCallback: true, // Allows passing req to callback
-//         },
-//         async (req, accessToken, refreshToken, profile, cb) => {
-//             try {
-//                 const role = req.query.role; // Extract role from state parameter
-
-//                 if (!role || (role !== "customer" && role !== "owner")) {
-//                     return cb(new Error("Invalid role specified"));
-//                 }
-
-//                 // Check if the user exists in the database
-//                 let user = await User.findOne({ email: profile.emails[0].value });
-
-//                 if (user) {
-//                     if (!alreadyExists(user, role)) {
-//                         await createRoleEntity(role, user, {});
-//                     }
-//                 } else {
-//                     // Create a new user
-//                     user = new User({
-//                         email: profile.emails[0].value,
-//                         roles: { [role]: true },
-//                         googleId: profile.id,
-//                     });
-
-//                     await user.save();
-//                 }
-
-//                 return cb(null, user);
-//             } catch (error) {
-//                 return cb(error);
-//             }
-//         }
-//     )
-// );
 
 
 passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/auth/google/callback",
-        scope: ["profile", "email"],
-        passReqToCallback: true,
-      },
-      async (req, accessToken, refreshToken, profile, cb) => {
-        try {
-          // Extract role from state parameter
-          const role = req.query.state;
-          if (!role || (role !== "customer" && role !== "owner")) {
-            return cb(new Error("Invalid role specified"));
-          }
-  
-          // The rest of your code remains the same
-          let user = await User.findOne({ email: profile.emails[0].value });
-  
-          if (user) {
-            if (!alreadyExists(user, role)) {
-              await createRoleEntity(role, user, {});
-            }
-          } else {
-            user = new User({
-              email: profile.emails[0].value,
-              roles: { [role]: true },
-              googleId: profile.id,
-            });
-  
-            await user.save();
-          }
-  
-          return cb(null, user);
-        } catch (error) {
-          return cb(error);
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/api/auth/google/callback",
+      scope: ["profile", "email"],
+      passReqToCallback: true,
+    },
+    async (req, accessToken, refreshToken, profile, cb) => {
+      try {
+        // Extract role from state parameter
+        const role = req.query.state;
+        if (!role || (role !== "customer" && role !== "owner")) {
+          return cb(new InvalidRoleError());
         }
+
+        // The rest of your code remains the same
+        let user = await User.findOne({ email: profile.emails[0].value });
+
+        const createRole = async (user) => {
+          if (role === 'owner') {
+            await createRoleEntity(role, user, { fname: profile.name.givenName, lname: profile.name.familyName });
+          } else {
+            await createRoleEntity(role, user, {});
+          }
+        }
+
+        if (user) {
+          if (!(await alreadyExists(user, role))) {
+            await createRole(user);
+          }
+        } else {
+          user = new User({
+            email: profile.emails[0].value,
+            googleId: profile.id,
+          });
+          await user.save();
+
+          await createRole(user);
+        }
+
+        return cb(null, user);
+      } catch (error) {
+        return cb(error);
       }
-    )
-  );
+    }
+  )
+);
 
 // Store user ID in session
 passport.serializeUser((user, done) => {
-    done(null, user._id);
+  done(null, user._id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        if (!user) return done(null, false);
+  try {
+    const user = await User.findById(id);
+    if (!user) return done(null, false);
 
-        const roles = await fetchUserRoles(id);
+    const roles = await fetchUserRoles(id);
 
-        done(null, { ...user.toObject(), roles });
-    } catch (error) {
-        done(error);
-    }
+    done(null, { ...user.toObject(), roles });
+  } catch (error) {
+    done(error);
+  }
 });
 
 
 // helper function
 
 const fetchUserRoles = async (userId) => {
-    const customer = await Customer.findOne({ user_id: userId });
-    const owner = await Owner.findOne({ user_id: userId });
+  const customer = await Customer.findOne({ user_id: userId });
+  const owner = await Owner.findOne({ user_id: userId });
 
-    return {
-        ...(customer && { customer }),
-        ...(owner && { owner }),
-    };
+  return {
+    ...(customer && { customer }),
+    ...(owner && { owner }),
+  };
 };
 
 export default passport;
