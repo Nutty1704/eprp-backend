e8680e44a9ec5efe72ee13c4560218fbca261261
Surendran Manokaran <surendranmelb24@gmail.com>
2025-03-24 13:29:10 +1100

    Added business controller and business route code (NOT TESTED)

---
+++ src/routes/business.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { uploadImg } from '../middlewares/multer.middleware.js';
+import businessController from '../controllers/business.controller.js';
+
+
+const router = express.Router();
+
+router.get("/", businessController.getMyBusiness);
+
+router.post(
+  "/",
+  uploadImg.single('profile_image'),
+  businessController.createMyBusiness
+);
+
+router.put(
+  "/",
+  uploadImg.single('profile_image'),
+  businessController.updateMyBusiness
+);
